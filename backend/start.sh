3c8f394262c967cb9dabb640d9b7c6a625fec294
NicolasSavin <42462652+NicolasSavin@users.noreply.github.com>
2026-03-12 20:47:35 +0400

    Update start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+# Обновляем pip и ставим зависимости
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 
+# Запуск FastAPI через uvicorn
 python -m uvicorn main:app --host 0.0.0.0 --port $PORT
